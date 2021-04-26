6a47479be24618a6e6c608fd3a606bea719f2db7
Dmytro Golovchenko <dmytro.golovchenko@gmail.com>
2021-04-25 23:28:25 -0700

    Added memory profing and tests on random lists to 47
    With number of sulutions n! we concerned about result size more than
    of generation of each individual result. Generating each result only
    once making huge difference, each duplicate number is cutting end result
    by factor of two.

--- backtracking_medium_47/README.md
+++ backtracking_medium_47/README.md
@@ -1,6 +1,14 @@
 # 47. Permutations II (Medium)
 
 Link to problem: https://leetcode.com/problems/permutations-ii/
-Link to best submission with frequency: https://leetcode.com/submissions/detail/485301136/
-Link to best submission with sort: https://leetcode.com/submissions/detail/485140295/
-Link to best submission with set: https://leetcode.com/submissions/detail/485130359/
+
+* Link to best submission with frequency: https://leetcode.com/submissions/detail/485301136/
+* Link to best submission with sort: https://leetcode.com/submissions/detail/485140295/
+* Link to best submission with set: https://leetcode.com/submissions/detail/485130359/
+
+
+You could run memory profiling as below (~6GB mem usage for size 11, ~72GB for size 12):
+```bash
+for i in {1..11}; do echo ${i}; time python3 backtracking_medium_47/permutations_ii_freq.py ${i} ${i}; done
+for i in {1..11}; do echo ${i}; time python3 backtracking_medium_47/permutations_ii_sorted.py ${i} ${i}; done
+```
--- backtracking_medium_47/permutations_ii_sorted.py
+++ backtracking_medium_47/permutations_ii_sorted.py
@@ -1,3 +1,9 @@
+import random
+import sys
+from typing import List
+from typing import Dict
+
+
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         def generate(nums: List[int], result, path):
@@ -13,3 +19,25 @@ class Solution:
         nums.sort()
         generate(nums, result, [])
         return result
+
+_UNIQUE_DEFAULT = 10
+_COUNT = 10
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        count = int(sys.argv[1])
+    else:
+        count = _COUNT
+    if len(sys.argv) > 2:
+        unique_count = min(int(sys.argv[2]), count)
+    else:
+        unique_count = min(_COUNT, count)
+
+    if count <= unique_count:
+        nums = random.sample(range(1, unique_count + 1), count)
+    else:
+        nums = random.choices(range(1, unique_count + 1), k=count)
+    result = Solution().permuteUnique(nums)
+    print('Result count:', len(result), f' Total size or result from {nums} in bytes is ', sys.getsizeof(result))
+    total_gb = sys.getsizeof(result) * sys.getsizeof(result[0]) / (8 * 1024 * 1024 * 1024)
+    print(f'Size of each premutation is about ', sys.getsizeof(result[0]), ' total mem usage in GB: ', total_gb)
