23816ad039b3b569c02769a4db300b8b8475195a
Dmytro Golovchenko <dmytro.golovchenko@gmail.com>
2021-04-25 22:04:33 -0700

    Add frequency solution for 1079. Letter Tile Possibilities

--- backtracking_medium_1079/README.md
+++ backtracking_medium_1079/README.md
@@ -1,5 +1,6 @@
 # 1079. Letter Tile Possibilities (Medium)
 
 Link to problem: https://leetcode.com/problems/letter-tile-possibilities/
+Link to best submission with frequency: https://leetcode.com/submissions/detail/485201608/
 Link to best submission with sort: https://leetcode.com/submissions/detail/485190987/
 Link to best submission with set: https://leetcode.com/submissions/detail/485131493/
---
+++ backtracking_medium_1079/letter_tile_possibilities_freq.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        """Use frequency count."""
+        def backtracking(frequencies: Dict[str, int]) -> int:
+            count = 0
+            for symbol in frequencies:
+                if frequencies[symbol] > 0:
+                    frequencies[symbol] -= 1
+                    count += 1 + backtracking(frequencies)
+                    frequencies[symbol] += 1
+            return count
+        
+        frequencies = {}
+        for symbol in tiles:
+            if symbol in frequencies:
+                frequencies[symbol] += 1
+            else:
+                frequencies[symbol] = 1
+        return backtracking(frequencies)
